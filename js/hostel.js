dbc49d3f7c5c72f878d4eef74a86654e43ca2078
uswa <uswanadeem242@gmail.com>
2023-03-22 13:12:05 +0500

    HBT

---
+++ js/hostel.js
@@ -0,0 +1,22 @@
+function iconOn(x){
+    x.style.height = "110px";
+    x.style.width = "110px";
+}
+function iconDown(x){
+    x.style.height = "100px";
+    x.style.width = "100px";
+}
+
+function myFunction(){
+    
+
+   document.getElementById("name").style.color = "#f7c08a";
+   document.getElementById('email').style.color = "#f7c08a";
+   document.getElementById("message").style.color = "#f7c08a";
+
+    // document.getElementById("name").style.color = "red";
+}
+
+
+
+
